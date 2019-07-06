b1ec109e2f54bf4646e62623f342b7c926362b28
jiangsinan <2459140452@qq.com>
2019-07-06 16:27:24 +0800

    初始化数据

--- user/models.py
+++ user/models.py
@@ -9,14 +9,29 @@ from lib.orm import ModelToDictMixin
 
 
 class Users(models.Model):
+    LOCATIONS = (
+        ('bj', '北京'),
+        ('sz', '深圳'),
+        ('sh', '上海'),
+        ('gz', '广州'),
+        ('cd', '成都'),
+        ('dl', '大连'),
+    )
+
+    SEXS = (
+        (0, '全部'),
+        (1, '男'),
+        (2, '女')
+    )
+
     phonenum = models.CharField(max_length=11,unique=True)
     nickname = models.CharField(max_length=32,null=True)
-    sex = models.IntegerField(default=0)
+    sex = models.IntegerField(default=0, choices=SEXS)
     birth_year = models.IntegerField(default=2000)
     birth_month = models.IntegerField(default=1)
     birth_day = models.IntegerField(default=1)
     avatar = models.CharField(max_length=256)
-    location = models.CharField(max_length=64,null=True)
+    location = models.CharField(max_length=64, choices=LOCATIONS)
 
     class Meta:
         db_table = 'users'
@@ -39,6 +54,7 @@ class Users(models.Model):
 
     def to_dict(self):
         return {
+            'id':self.id,
             'phonenum': self.phonenum,
             'nickname': self.nickname,
             'sex': self.sex,
@@ -54,6 +70,9 @@ class Profile(models.Model,ModelToDictMixin):
         ('bj', '北京'),
         ('sz', '深圳'),
         ('sh', '上海'),
+        ('gz', '广州'),
+        ('cd', '成都'),
+        ('dl', '大连'),
     )
 
     SEXS = (
